48069ee3d7d294c60e3ffbf2c8f4c5f73bba49fe
Nastya Stavitskaya <nastycorps@gmail.com>
2017-02-10 18:28:49 -0800

    Add scripts

---
+++ src/js/custom.js
@@ -0,0 +1,37 @@
+var homeContent = document.getElementById("homeContent");
+var searchContent = document.getElementById("searchContent");
+var aboutContent = document.getElementById("aboutContent");
+
+document.getElementById("home").addEventListener("click", function(){
+  // onMenuItemClick("homeContent"); ???
+  homeContent.classList.add("hide");
+  homeContent.classList.remove("active");
+  searchContent.classList.add("hide");
+  searchContent.classList.remove("active");
+  aboutContent.classList.add("hide");
+  aboutContent.classList.remove("active");
+  homeContent.classList.remove("hide");
+  homeContent.classList.add("active");
+})
+
+document.getElementById("search").addEventListener("click", function(){
+  homeContent.classList.add("hide");
+  homeContent.classList.remove("active");
+  searchContent.classList.add("hide");
+  searchContent.classList.remove("active");
+  aboutContent.classList.add("hide");
+  aboutContent.classList.remove("active");
+  searchContent.classList.remove("hide");
+  searchContent.classList.add("active");
+})
+
+document.getElementById("about").addEventListener("click", function(){
+  homeContent.classList.add("hide");
+  homeContent.classList.remove("active");
+  searchContent.classList.add("hide");
+  searchContent.classList.remove("active");
+  aboutContent.classList.add("hide");
+  aboutContent.classList.remove("active");
+  aboutContent.classList.remove("hide");
+  aboutContent.classList.add("active");
+})
