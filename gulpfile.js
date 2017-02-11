a15f63daf9a6b8e4446c08698dc4c8f412edb983
Nastya Stavitskaya <nastycorps@gmail.com>
2017-02-10 18:55:38 -0800

    Add /build/js/app.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
-var cssmin = require('gulp-cssmin');
 var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
 
 gulp.task('app-styles', function(){
   return gulp.src('src/styles/*.css')
     .pipe(cssmin())
     .pipe(concat('app.min.css'))
     .pipe(gulp.dest('build/styles'));
+});
+
+gulp.task('app-scripts', function(){
+  return gulp.src('src/js/*.js')
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('build/js'));
 });
