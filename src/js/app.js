6faf3035cdd9ca03165ecfe0609b9a741a343469
Nastya Stavitskaya <nastycorps@gmail.com>
2017-03-01 19:48:11 -0800

    Modified handler.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,47 +1,14 @@
 import { tokenList, elements } from './constants';
 import { default as onMenuItemClick } from './handler';
-import { runAjax } from './ajax';
-
-
 
 (elements[tokenList[0]].menu).click(function(){
   onMenuItemClick(0);
-
-  function successHandler(data){
-    let div = document.createElement('div');
-        div.className = "alert alert-success alert-dismissable";
-        div.innerHTML = "<a href='#'' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>" + data + "</strong>";
-        setTimeout(function(){ div.remove() }, 3000);
-    $('#homeContent').append(div);
-  };
-
-  runAjax('/api/home', successHandler);
 });
 
 (elements[tokenList[1]].menu).click(function(){
   onMenuItemClick(1);
-
-  function successHandler(data){
-    let div = document.createElement('div');
-        div.className = "alert alert-success alert-dismissable";
-        div.innerHTML = "<a href='#'' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>" + data + "</strong>";
-        setTimeout(function(){ div.remove() }, 3000);
-    $('#searchContent').append(div);
-  };
-
-  runAjax('/api/search', successHandler);
 });
 
 (elements[tokenList[2]].menu).click(function(){
   onMenuItemClick(2);
-
-  function successHandler(data){
-    let div = document.createElement('div');
-        div.className = "alert alert-success alert-dismissable";
-        div.innerHTML = "<a href='#'' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>" + data + "</strong>";
-        setTimeout(function(){ div.remove() }, 3000);
-    $('#aboutContent').append(div);
-  };
-
-  runAjax('/api/about', successHandler);
 });
--- src/js/handler.js
+++ src/js/handler.js
@@ -1,4 +1,5 @@
 import { tokenList, elements } from './constants';
+import { runAjax } from './ajax';
 
 var activeIndex = 0;
 
@@ -8,4 +9,16 @@ export default function handler(newIndex){
   (elements[tokenList[newIndex]].menu).addClass("active");
   (elements[tokenList[newIndex]].content).removeClass("hide");
   activeIndex = newIndex;
+  function successHandler(data){
+    let div = document.createElement('div');
+        div.className = "alert alert-success alert-dismissable";
+        div.innerHTML = "<a href='#'' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>" + data + "</strong>";
+        setTimeout(function(){ div.remove() }, 3000);
+    (elements[tokenList[newIndex]].content).append(div);
+  }
+  runAjax('/api/' + tokenList[newIndex], successHandler);
 }
+
+
+
+
